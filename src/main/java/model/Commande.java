4ab3fc8238b02b091da3707f606b1e655b9b9b38
BALOG Israël Christian <balog@et.esiea.fr>
2024-01-12 10:46:11 +0100

    Initial commit

---
+++ src/main/java/model/Commande.java
@@ -0,0 +1,31 @@
+package model;
+
+
+public class Commande {
+    private int id;
+    private String nom;
+
+    public Commande() {
+    }
+
+    public Commande(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+}
